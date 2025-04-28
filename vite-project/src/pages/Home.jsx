d5a63124d142df441665243fa895f0b342111896
Sumit6307 <sumitkesar6307@gmail.com>
2025-04-28 23:56:11 +0530

    fix: Remove developer count display from pagination in Home component

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -394,7 +394,8 @@ function Home() {
                 </motion.button>
               </div>
               <p className="text-center text-gray-400 mt-4">
-                Page {currentPage} of {totalPages} | Showing {filteredProfiles.length} developers
+                Page {currentPage} of {totalPages} 
+                {/* | Showing {filteredProfiles.length} developers */}
               </p>
             </>
           ) : (
